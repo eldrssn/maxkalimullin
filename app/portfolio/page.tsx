8cce6f9a2b820b24aa62f1e65ad152b5e018a83c
eldrssn <maxkalimullin@gmail.com>
2023-05-18 23:59:07 +0530

    add portfolio btn and change themeswitcher

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
+import Experience from 'components/base/experience';
+
+export const metadata = {
+  title: 'Portfolio',
+};
+
+const page = () => <Experience />;
+
+export default page;
